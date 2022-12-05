2eec17c3d9529f46e8945b8327ea337dd152d391
sumi.byun <selina2000@naver.com>
2022-12-06 00:32:45 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -5,17 +5,16 @@ N_MAX = 13
 if __name__ == "__main__":
     # sys.stdin = open("index.txt", "r")
 
-    T = int(sys.stdin.readline().rstrip())
+    T = int(sys.stdin.readline())
 
-    dp = [0,1,2,4]
+    dp = [1,2,4]
 
-    for x in range(4, N_MAX):
-        dp.append(dp[x-1]+ dp[x-2]+ dp[x-3])
+    for x in range(4, 12):
+        dp.append(sum(dp[-3:]))
 
     for _ in range(T):
-        n = int(sys.stdin.readline().rstrip())
-        print(dp[n])
-
+        n = int(sys.stdin.readline())
+        print(dp[n-1])
 
 
 
