f5f992cdd6b0ef5756cb56a35864882614a6505a
sumi.byun <selina2000@naver.com>
2022-12-05 21:57:44 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,34 @@
+import sys
+
+def fibo(x):
+    # 탑다운
+    # 종료 조건
+    if x==1 or x==2:
+        return 1
+
+    if dp[x] !=0:
+        return dp[x]
+
+    dp[x] = fibo(x-1) + fibo(x-2)
+    return dp[x]
+
+def fibo_bottom(n):
+    dp = [0] * 100
+
+    dp[1] = 1
+    dp[2] = 1
+
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n]
+
+if __name__ == "__main__":
+    #sys.stdin = open("index.txt", "r")
+
+    n = int(sys.stdin.readline().rstrip())
+
+    dp = [0] * 90
+
+    print(fibo_bottom(n))
+
