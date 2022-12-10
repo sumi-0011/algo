ceae4854e48cf1dc3035bb53cf33bd4d6438caf0
sumi.byun <selina2000@naver.com>
2022-12-10 15:59:11 +0900

    [Bronze V] Title: 새싹, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    # sys.stdin = open("index.txt", "r")
+    print("         ,r\'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \. \". L_r\'")
+    print("   `~\/")
+    print("      |")
+    print("      |")
