1e7bc5a64c4a1ca11b14a3c12a80127df67b26c3
sumi.byun <selina2000@naver.com>
2022-12-24 22:23:22 +0900

    [Bronze V] Title: Pyramids, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5341. Pyramids/Pyramids.py
+++ 백준/Bronze/5341. Pyramids/Pyramids.py
@@ -3,10 +3,6 @@ import sys
 while True:
     N = int(sys.stdin.readline())
 
-    if N == 0:
-        break;
-    answer = 0
-    for i in range(1, N + 1):
-        answer += i
-    print(answer)
+    if N == 0: break
+    print(N*(N+1)//2)
 #
