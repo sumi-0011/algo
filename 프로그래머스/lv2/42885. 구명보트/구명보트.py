2b0b7706fda8a06558749b004803aef7fb9424c8
sumi.byun <selina2000@naver.com>
2023-01-16 22:06:35 +0900

    [level 2] Title: 구명보트, Time: 10.87 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+
+def solution(people, limit):
+    answer = 0
+    
+    queue = deque()
+    people.sort()
+    for x in people:
+        queue.append(x)
+    
+    while queue:
+        tr = queue.pop()
+        if not queue:
+            answer+=1
+            break;
+        tl = queue.popleft()
+        if tr + tl > limit:
+            answer +=1
+            queue.appendleft(tl)
+        else:
+            answer+=1
+    print(*queue)
+    return answer
