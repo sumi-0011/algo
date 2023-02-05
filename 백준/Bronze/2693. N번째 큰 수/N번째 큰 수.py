29b41bfbf5c88f8ae46bbee15b02f086b5c5ce71
sumi.byun <selina2000@naver.com>
2023-02-05 16:50:19 +0900

    [Bronze I] Title: N번째 큰 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,17 @@
+import sys 
+input = sys.stdin.readline
+
+MAX_SIZE = 1000000
+
+
+def solution(arr):
+    arr.sort(reverse=True)
+    return arr[2]
+
+
+if __name__ == "__main__":
+    n   = int(input())
+    for _ in range(n):
+        arr = list(map(int, input().split()))
+        res = solution(arr)
+        print(res)
