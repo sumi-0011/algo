1055fd387c58a91f54490e2cc2d53f8e72e278d9
sumi.byun <selina2000@naver.com>
2023-02-07 15:25:54 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,11 @@
 def solution(s1, s2):
-    answer = 0
-    for x in s1:
-        for y in s2:
-            if x == y:
-                answer +=1
-    return answer
+    # answer = 0
+    # for x in s1:
+    #     for y in s2:
+    #         if x == y:
+    #             answer +=1
+    l1 = len(s1)
+    l2 = len(s2)
+    arr = set(s1+s2)
+    
+    return l1 + l2 - len(arr)
