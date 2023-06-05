84be844498134ea3bb8d1e663802a53a9a3f90cb
sumi.byun <selina2000@naver.com>
2023-06-05 17:07:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.04 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    prev = -1;
+    
+    for x in arr:
+        if x == prev:
+            continue
+        answer.append(x)
+        prev = x
+    
+    return answer
+        
